8d357915ab32ee7f2b84b353bd9784db8875de2d
ansarmca <ansarkp300@gmail.com>
2024-10-18 11:44:32 +0530

    Update merge_2_sorted_arrays.c

--- merge_2_sorted_arrays.c
+++ merge_2_sorted_arrays.c
@@ -6,7 +6,6 @@ void sortArray(int arr[], int size) {
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap elements
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
