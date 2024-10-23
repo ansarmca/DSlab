1eecf5709618cb96fc9f62e5a7468d83496f8a2c
ansarmca <ansarkp300@gmail.com>
2024-10-23 15:12:05 +0530

    Update circular_queue.c

--- circular_queue.c
+++ circular_queue.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-#define MAX_SIZE 5
+#define SIZE 5
 
-    int queue[MAX_SIZE];
+int queue[SIZE];
 int front = -1, rear = -1;
-
 
 int isFull()
 {
-    return (rear + 1) % MAX_SIZE == front;
+    return (rear + 1) % SIZE == front;
 }
 
 int isEmpty()
@@ -26,15 +25,13 @@ void enqueue(int data)
     if (front == -1) {
         front = 0;
     }
-    rear = (rear + 1) % MAX_SIZE;
+    rear = (rear + 1) % SIZE;
     queue[rear] = data;
     printf("Element %d inserted\n", data);
 }
 
-
 int dequeue()
 {
-
     if (isEmpty()) {
         printf("Queue underflow\n");
         return -1;
@@ -42,16 +39,14 @@ int dequeue()
     int data = queue[front];
     if (front == rear) {
         front = rear = -1;
-    }
-    else {
-        front = (front + 1) % MAX_SIZE;
+    } else {
+        front = (front + 1) % SIZE;
     }
     return data;
 }
 
 void display()
 {
-
     if (isEmpty()) {
         printf("Queue is empty\n");
         return;
@@ -60,9 +55,8 @@ void display()
     int i = front;
     while (i != rear) {
         printf("%d ", queue[i]);
-        i = (i + 1) % MAX_SIZE;
+        i = (i + 1) % SIZE;
     }
-
     printf("%d\n", queue[rear]);
 }
 
@@ -79,4 +73,3 @@ int main()
 
     return 0;
 }
-```
