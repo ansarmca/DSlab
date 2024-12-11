fd16500c94708573e6e731ac561ad24f2b510bbf
ansarmca <ansarkp300@gmail.com>
2024-12-11 15:39:24 +0530

    Update set_operations.c

--- set_operations.c
+++ set_operations.c
@@ -1,139 +1,111 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-void bitVector(char set[], int bit_vector[], char U[])
-{
-    for (int i = 0; i < 26; i++)
-    {
-        bit_vector[i] = 0;
-    }
-    for (int i = 0; set[i] != '\0'; i++)
-    {
-        for (int j = 0; j < 26; j++)
-        {
-            if (set[i] == U[j])
-            {
-                bit_vector[j] = 1;
-                break;
-            }
+#define UNIVERSAL_SIZE 26
+
+void createBitVector(char set[], int bit_vector[]) {
+    memset(bit_vector, 0, UNIVERSAL_SIZE * sizeof(int));
+    for (int i = 0; set[i] != '\0'; i++) {
+        if (isalpha(set[i])) {
+            bit_vector[tolower(set[i]) - 'a'] = 1;
         }
     }
 }
 
-void printBitVector(int bit_vector[])
-{
-    for (int i = 0; i < 26; i++)
-    {
+void printBitVector(int bit_vector[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
         printf("%d", bit_vector[i]);
     }
     printf("\n");
 }
 
-void union_operation(int set1_bit[], int set2_bit[], int result[])
-{
-    for (int i = 0; i < 26; i++)
-        result[i] = set1_bit[i] | set2_bit[i];
-}
-
-void intersection(int set1_bit[], int set2_bit[], int result[])
-{
-    for (int i = 0; i < 26; i++)
-    {
-        result[i] = set1_bit[i] & set2_bit[i];
+void performUnion(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] | set2[i];
     }
 }
 
-void complementOperation(int set_bit[], int result[])
-{
-    for (int i = 0; i < 26; i++)
-    {
-        result[i] = (set_bit[i] == 0) ? 1 : 0;
+void performIntersection(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] & set2[i];
     }
 }
 
-void difference(int set1_bit[], int set2_bit[], int result[])
-{
-    int complement[26];
-    complementOperation(set2_bit, complement);
-    for (int i = 0; i < 26; i++)
-        result[i] = set1_bit[i] & set2_bit[i];
+void performComplement(int set[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = !set[i];
+    }
 }
 
-int main()
-{
-    char Universal_set[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    char set1[26], set2[26];
-    int set1_bit[26], set2_bit[26], result[26];
-    int set1_size, set2_size;
-
-    printf("Enter size of Set 1: ");
-    scanf("%d", &set1_size);
-    printf("\nEnter elements for Set 1: ");
-    for (int i = 0; i < set1_size; i++)
-    {
-        scanf(" %c", &set1[i]);
-        set1[i] = tolower(set1[i]);
+void performDifference(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] & !set2[i];
     }
-    set1[set1_size] = '\0';
+}
 
-    printf("\nEnter size of Set 2: ");
-    scanf("%d", &set2_size);
-    printf("\nEnter elements for Set 2: ");
-    for (int i = 0; i < set2_size; i++)
-    {
-        scanf(" %c", &set2[i]);
-        set2[i] = tolower(set2[i]);
-    }
-    set2[set2_size] = '\0';
+int main() {
+    char set1[UNIVERSAL_SIZE + 1], set2[UNIVERSAL_SIZE + 1];
+    int set1_bit[UNIVERSAL_SIZE], set2_bit[UNIVERSAL_SIZE], result[UNIVERSAL_SIZE];
+    int choice;
 
-    bitVector(set1, set1_bit, Universal_set);
-    printf("\nBit Vector Representation of Set 1: \n");
+    printf("Enter elements for Set 1 (lowercase letters only): ");
+    scanf("%s", set1);
+    printf("Enter elements for Set 2 (lowercase letters only): ");
+    scanf("%s", set2);
+
+    createBitVector(set1, set1_bit);
+    createBitVector(set2, set2_bit);
+
+    printf("\nBit Vector of Set 1:\n");
     printBitVector(set1_bit);
-
-    bitVector(set2, set2_bit, Universal_set);
-    printf("\nBit Vector Representation of Set 2: \n");
+    printf("Bit Vector of Set 2:\n");
     printBitVector(set2_bit);
 
-    int choice;
-    while (choice != 5)
-    {
-        printf("\n 1.Set Union\n 2.Intersection\n 3.Complement\n 4.Set Difference\n 5.Exit\n");
-        printf("Enter Your Choice Here: ");
+    do {
+        printf("\nOperations:\n");
+        printf("1. Union\n");
+        printf("2. Intersection\n");
+        printf("3. Complement\n");
+        printf("4. Difference (Set1 - Set2)\n");
+        printf("5. Exit\n");
+        printf("Choose an option: ");
         scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            union_operation(set1_bit, set2_bit, result);
-            printf("\nUnion of Set 1 and Set 2 is :\n");
-            printBitVector(result);
-            break;
-        case 2:
-            intersection(set1_bit, set2_bit, result);
-            printf("\nIntersection of Set 1 and Set 2 is :\n");
-            printBitVector(result);
-            break;
-        case 3:
-            complementOperation(set1_bit, result);
-            printf("\nComplement of Set 1 is :\n");
-            printBitVector(result);
-            complementOperation(set2_bit, result);
-            printf("\nComplement of Set 2 is :\n");
-            printBitVector(result);
-            break;
-        case 4:
-            difference(set1_bit, set2_bit, result);
-            printf("\nSet Difference S1 - S2 is :\n");
-    	    printBitVector(result);
-    	    difference(set2_bit, set1_bit, result);
-    	    printf("\nSet Difference S2 - S1 is :\n");
-            printBitVector(result);
-            break;
-        case 5:
-            return 0;
-        default:
-            printf("\nInvalid Choice, Try Again\n");
-        }
-    }
 
-    return (0);
+        switch (choice) {
+            case 1:
+                performUnion(set1_bit, set2_bit, result);
+                printf("\nUnion:\n");
+                printBitVector(result);
+                break;
+            case 2:
+                performIntersection(set1_bit, set2_bit, result);
+                printf("\nIntersection:\n");
+                printBitVector(result);
+                break;
+            case 3:
+                performComplement(set1_bit, result);
+                printf("\nComplement of Set 1:\n");
+                printBitVector(result);
+                performComplement(set2_bit, result);
+                printf("\nComplement of Set 2:\n");
+                printBitVector(result);
+                break;
+            case 4:
+                performDifference(set1_bit, set2_bit, result);
+                printf("\nDifference (Set1 - Set2):\n");
+                printBitVector(result);
+                performDifference(set2_bit, set1_bit, result);
+                printf("\nDifference (Set2 - Set1):\n");
+                printBitVector(result);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
 }
