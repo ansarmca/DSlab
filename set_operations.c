a0a390baf64e5bbc1462cfe38a8f21d021b012b0
ansarmca <ansarkp300@gmail.com>
2024-12-11 13:47:44 +0530

    Update set_operations.c

--- set_operations.c
+++ set_operations.c
@@ -3,90 +3,84 @@
 
 #define UNIVERSAL_SIZE 26
 
-void inputSet(int set[]) {
-    char input[100];
-    printf("Enter elements of the set (without spaces, lowercase a-z): ");
-    scanf("%s", input);
-
-    for (int i = 0; i < strlen(input); i++) {
-        set[input[i] - 'a'] = 1;
-    }
-}
-
-void displaySet(int set[]) {
+// Convert a bit vector to a set representation and display it
+void displaySetFromBitVector(unsigned int set) {
     printf("{ ");
     for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        if (set[i]) {
+        if (set & (1 << i)) {
             printf("%c ", i + 'a');
         }
     }
     printf("}\n");
 }
 
-void unionSet(int set1[], int set2[], int result[]) {
-    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        result[i] = set1[i] | set2[i];
+// Input elements and set the corresponding bits in the bit vector
+unsigned int inputSetAsBitVector() {
+    unsigned int set = 0;
+    char input[100];
+    printf("Enter elements of the set (without spaces, lowercase a-z): ");
+    scanf("%s", input);
+
+    for (int i = 0; i < strlen(input); i++) {
+        set |= (1 << (input[i] - 'a'));
     }
+
+    return set;
 }
 
-void intersectionSet(int set1[], int set2[], int result[]) {
-    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        result[i] = set1[i] & set2[i];
-    }
+// Bitwise operations for union, intersection, and difference
+unsigned int unionSetBitVector(unsigned int set1, unsigned int set2) {
+    return set1 | set2;
 }
 
-void differenceSet(int set1[], int set2[], int result[]) {
-    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        result[i] = set1[i] & !set2[i];
-    }
+unsigned int intersectionSetBitVector(unsigned int set1, unsigned int set2) {
+    return set1 & set2;
 }
 
-void complementSet(int set[], int universalSet[], int result[]) {
-    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        result[i] = universalSet[i] & !set[i];
-    }
+unsigned int differenceSetBitVector(unsigned int set1, unsigned int set2) {
+    return set1 & ~set2;
+}
+
+unsigned int complementSetBitVector(unsigned int set, unsigned int universalSet) {
+    return ~set & universalSet;
 }
 
 int main() {
-    int universalSet[UNIVERSAL_SIZE] = {1};
-    int set1[UNIVERSAL_SIZE] = {0}, set2[UNIVERSAL_SIZE] = {0};
-    int result[UNIVERSAL_SIZE] = {0};
-    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        universalSet[i] = 1;
-    }
+    unsigned int universalSet = (1 << UNIVERSAL_SIZE) - 1; // All 26 bits set
+    unsigned int set1 = 0, set2 = 0, result = 0;
 
     printf("Universal set: { a b c d e f g h i j k l m n o p q r s t u v w x y z }\n");
 
     printf("Input Set 1:\n");
-    inputSet(set1);
+    set1 = inputSetAsBitVector();
 
     printf("Input Set 2:\n");
-    inputSet(set2);
+    set2 = inputSetAsBitVector();
 
     printf("\nSet 1: ");
-    displaySet(set1);
+    displaySetFromBitVector(set1);
     printf("Set 2: ");
-    displaySet(set2);
+    displaySetFromBitVector(set2);
 
-    unionSet(set1, set2, result);
+    result = unionSetBitVector(set1, set2);
     printf("\nUnion of Set 1 and Set 2: ");
-    displaySet(result);
+    displaySetFromBitVector(result);
 
-    intersectionSet(set1, set2, result);
+    result = intersectionSetBitVector(set1, set2);
     printf("Intersection of Set 1 and Set 2: ");
-    displaySet(result);
+    displaySetFromBitVector(result);
 
-    differenceSet(set1, set2, result);
+    result = differenceSetBitVector(set1, set2);
     printf("Difference of Set 1 and Set 2 (Set1 - Set2): ");
-    displaySet(result);
+    displaySetFromBitVector(result);
 
-    complementSet(set1, universalSet, result);
+    result = complementSetBitVector(set1, universalSet);
     printf("Complement of Set 1: ");
-    displaySet(result);
+    displaySetFromBitVector(result);
 
-    complementSet(set2, universalSet, result);
+    result = complementSetBitVector(set2, universalSet);
     printf("Complement of Set 2: ");
-    displaySet(result);
+    displaySetFromBitVector(result);
 
     return 0;
 }
