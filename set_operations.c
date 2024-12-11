3b79dc3dd242a83b2caa7480e3a55fdcca0445bf
ansarmca <ansarkp300@gmail.com>
2024-12-11 13:44:35 +0530

    Update set_operations.c

--- set_operations.c
+++ set_operations.c
@@ -3,91 +3,90 @@
 
 #define UNIVERSAL_SIZE 26
 
-// Function to input a set and convert it to a bit vector
-int inputSet() {
+void inputSet(int set[]) {
     char input[100];
-    int set = 0; // Bit vector initialized to 0
     printf("Enter elements of the set (without spaces, lowercase a-z): ");
     scanf("%s", input);
 
     for (int i = 0; i < strlen(input); i++) {
-        set |= (1 << (input[i] - 'a')); // Set the bit corresponding to the character
+        set[input[i] - 'a'] = 1;
     }
-    return set;
 }
 
-// Function to display a set from its bit vector representation
-void displaySet(int set) {
+void displaySet(int set[]) {
     printf("{ ");
     for (int i = 0; i < UNIVERSAL_SIZE; i++) {
-        if (set & (1 << i)) {
+        if (set[i]) {
             printf("%c ", i + 'a');
         }
     }
     printf("}\n");
 }
 
-// Function to perform union of two sets
-int unionSet(int set1, int set2) {
-    return set1 | set2; // Bitwise OR
+void unionSet(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] | set2[i];
+    }
 }
 
-// Function to perform intersection of two sets
-int intersectionSet(int set1, int set2) {
-    return set1 & set2; // Bitwise AND
+void intersectionSet(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] & set2[i];
+    }
 }
 
-// Function to perform difference of two sets (set1 - set2)
-int differenceSet(int set1, int set2) {
-    return set1 & ~set2; // Bitwise AND with complement
+void differenceSet(int set1[], int set2[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = set1[i] & !set2[i];
+    }
 }
 
-// Function to perform complement of a set with respect to the universal set
-int complementSet(int set) {
-    int universalSet = (1 << UNIVERSAL_SIZE) - 1; // All bits set for a-z
-    return universalSet & ~set; // Bitwise AND with complement
+void complementSet(int set[], int universalSet[], int result[]) {
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        result[i] = universalSet[i] & !set[i];
+    }
 }
 
 int main() {
+    int universalSet[UNIVERSAL_SIZE] = {1};
+    int set1[UNIVERSAL_SIZE] = {0}, set2[UNIVERSAL_SIZE] = {0};
+    int result[UNIVERSAL_SIZE] = {0};
+    for (int i = 0; i < UNIVERSAL_SIZE; i++) {
+        universalSet[i] = 1;
+    }
+
     printf("Universal set: { a b c d e f g h i j k l m n o p q r s t u v w x y z }\n");
 
-    // Input sets
     printf("Input Set 1:\n");
-    int set1 = inputSet();
+    inputSet(set1);
 
     printf("Input Set 2:\n");
-    int set2 = inputSet();
+    inputSet(set2);
 
     printf("\nSet 1: ");
     displaySet(set1);
-
     printf("Set 2: ");
     displaySet(set2);
 
-    // Union
-    int unionResult = unionSet(set1, set2);
+    unionSet(set1, set2, result);
     printf("\nUnion of Set 1 and Set 2: ");
-    displaySet(unionResult);
+    displaySet(result);
 
-    // Intersection
-    int intersectionResult = intersectionSet(set1, set2);
+    intersectionSet(set1, set2, result);
     printf("Intersection of Set 1 and Set 2: ");
-    displaySet(intersectionResult);
+    displaySet(result);
 
-    // Difference
-    int differenceResult = differenceSet(set1, set2);
+    differenceSet(set1, set2, result);
     printf("Difference of Set 1 and Set 2 (Set1 - Set2): ");
-    displaySet(differenceResult);
+    displaySet(result);
 
-    // Complement of Set 1
-    int complementResult1 = complementSet(set1);
+    complementSet(set1, universalSet, result);
     printf("Complement of Set 1: ");
-    displaySet(complementResult1);
+    displaySet(result);
 
-    // Complement of Set 2
-    int complementResult2 = complementSet(set2);
+    complementSet(set2, universalSet, result);
     printf("Complement of Set 2: ");
-    displaySet(complementResult2);
+    displaySet(result);
 
     return 0;
 }
